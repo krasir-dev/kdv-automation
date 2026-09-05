d29ae87e71bc16e1b921627eaa2f292aa42c77a4
Илья Шкловский <krasir@mail.ru>
2026-09-05 08:51:11 +0800

    Remove test.py from repository

--- test.py
+++
@@ -1,6 +0,0 @@
-# Создайте временный файл test.py
-import selenium
-import openpyxl
-print("Selenium version:", selenium.__version__)
-print("Openpyxl version:", openpyxl.__version__)
-print("Все библиотеки установлены успешно!")
